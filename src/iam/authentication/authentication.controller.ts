f255aa87b97190bb876ef6ab4ed11f3858ed0ade
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-01 15:34:10 -0300

    feat: implement AuthenticationController signUp and signIn

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { SignUpDto } from './dto/sign-up/sign-up.dto';
+import { SignInDto } from './dto/sign-in/sign-in.dto';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Post('/sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authenticationService.signUp(signUpDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authenticationService.signIn(signInDto);
+  }
+}

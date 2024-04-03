774b7904154b736d0eada5ef998ac7d669353321
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-03 14:32:11 -0300

    refactor: implement RefreshTokenIdsStorage with setup methods integrated into NestJS lifecycle.

---
+++ src/iam/authentication/refresh-token-ids.storage/refresh-token-ids.storage.ts
@@ -0,0 +1,26 @@
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RefreshTokenIdsStorage
+  implements OnApplicationBootstrap, OnApplicationShutdown
+{
+  private redisClient: Redis;
+
+  onApplicationBootstrap(): any {
+    // TODO: Move this to a dedicated "RedisModule" instead of initiating the connection here
+    this.redisClient = new Redis({
+      // TODO: Use environment variables instead
+      host: 'localhost',
+      port: 6379,
+    });
+  }
+
+  onApplicationShutdown(signal?: string) {
+    return this.redisClient.quit();
+  }
+}

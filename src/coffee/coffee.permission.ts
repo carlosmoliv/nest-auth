2ef94935dc0b0834f339cf91f6d849bd620cedd7
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-04 15:26:25 -0300

    feat: add CoffeePermission enum

---
+++ src/coffee/coffee.permission.ts
@@ -0,0 +1,6 @@
+// TODO: store these permissions in a database table instead
+export enum CoffeePermission {
+  CreateCoffee = 'create_coffee',
+  UpdateCoffee = 'update_coffee',
+  DeleteCoffee = 'delete_coffee',
+}

ea02a86430c0ad19c0afe40f110de9bafaec03f1
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-04 15:29:07 -0300

    feat: implement PermissionsGuard for permission-based access control

---
+++ src/iam/authorization/guards/permissions.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ActiveUserData } from '../../interfaces/active-user-data.interface';
+import { REQUEST_USER_KEY } from '../../iam.constants';
+import { PermissionType } from '../permission.type';
+import { PERMISSIONS_KEY } from '../decorators/permissions.decorator';
+
+@Injectable()
+export class PermissionsGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const contextPermissions = this.reflector.getAllAndOverride<
+      PermissionType[]
+    >(PERMISSIONS_KEY, [context.getHandler(), context.getClass()]);
+    if (!contextPermissions) return true;
+    const user: ActiveUserData = context.switchToHttp().getRequest()[
+      REQUEST_USER_KEY
+    ];
+    return contextPermissions.every((permission) =>
+      user.permissions?.includes(permission),
+    );
+  }
+}

2e05fd792577a09cd89bec5dc87495d8d24bdd31
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-04 11:57:57 -0300

    feat: restrict create and remove coffee resources using the Roles decorator

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -12,11 +12,14 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { ActiveUser } from '../iam/decorators/active-user.decorator';
 import { ActiveUserData } from '../iam/interfaces/active-user-data.interface';
+import { Roles } from '../iam/authorization/roles.decorator';
+import { Role } from 'src/user/enums/role.enum';
 
 @Controller('coffees')
 export class CoffeeController {
   constructor(private readonly coffeeService: CoffeeService) {}
 
+  @Roles(Role.Admin)
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     return this.coffeeService.create(createCoffeeDto);
@@ -38,6 +41,7 @@ export class CoffeeController {
     return this.coffeeService.update(+id, updateCoffeeDto);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.coffeeService.remove(+id);
